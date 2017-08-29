63ad3d8f77b553416a3a0ad2c13ea29bb609c13d
Ox Cart <ox.to.a.cart@gmail.com>
2017-08-28 22:08:26 -0500

    Cleaner wait-and-cleanup logic for Windows

--- sysproxy_windows.go
+++ sysproxy_windows.go
@@ -4,8 +4,14 @@ package sysproxy
 
 import (
 	"os/exec"
+	"syscall"
 )
 
+const DETACHED_PROCESS = 0x00000008
+const CREATE_NEW_PROCESS_GROUP = 0x00000200
+
 func detach(cmd *exec.Cmd) {
-	// on Windows, we don't have to do anything special to detach process
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		CreationFlags: CREATE_NEW_PROCESS_GROUP,
+	}
 }
