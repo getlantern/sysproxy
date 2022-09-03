03996311f1c95bba7436d8a27c840a1fd16d380d
Myles Horton <myles@getlantern.org>
2022-09-03 17:24:46 -0400

    refactor build tags to be more standard

--- sysproxy_windows_amd64.go
+++ sysproxy_windows_amd64.go
@@ -9,6 +9,10 @@ import (
 //go:embed binaries/windows/sysproxy_amd64.exe
 var sysproxy []byte
 
+func ensureElevatedOnDarwin(be *byteexec.Exec, prompt string, iconFullPath string) (err error) {
+	return nil
+}
+
 func detach(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
