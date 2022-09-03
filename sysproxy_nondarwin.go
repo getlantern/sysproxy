ff498f5adebbf2cb405d260e004415f5298bd2f9
Myles Horton <myles@getlantern.org>
2022-09-03 13:10:37 -0400

    define sysproxy

--- sysproxy_nondarwin.go
+++ sysproxy_nondarwin.go
@@ -6,6 +6,8 @@ import (
 	"github.com/getlantern/byteexec"
 )
 
+var sysproxy []byte
+
 func ensureElevatedOnDarwin(be *byteexec.Exec, prompt string, iconFullPath string) (err error) {
 	return nil
 }
